83d1109300a1551b7f63114735b4e8220b02a1d2
ChauNguyen97 <50213352+ChauNguyen97@users.noreply.github.com>
2022-02-17 22:49:55 +0700

    add login and regis page, fail roter dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import Home from "./page/home/Home"
-import TopBar from "./components/topbar/TopBar"
-import Write from "./page/write/Write"
-import Settings from "./page/settings/Settings"
+import Home from "./page/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Write from "./page/write/Write";
+import Settings from "./page/settings/Settings";
+import Login from "./page/login/Login";
+import Register from "./page/register/Register";
+import { BrowserRouter as Routers,Router, Route, Link } from "react-router-dom";
 
 function App() {
-    return ( <
-        >
-        <
-        TopBar / >
-        <
-        Settings / > < / >
-    );
+  return (
+    <Router>
+      <TopBar />
+      <Routers>
+        <Route extract path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        
+      </Routers>
+    </Router>
+  );
 }
 
 export default App;
